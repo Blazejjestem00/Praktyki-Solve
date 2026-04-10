869d358f70106e8f239026814c575aa501d973d8
makaprii <franek_zygmunt@o2.pl>
2026-04-10 11:42:42 +0200

    T-5 added information window

--- tinder-clone/src/components/mainView/mainView.tsx
+++ tinder-clone/src/components/mainView/mainView.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Info from '../info/info'
 
 
 function MainView() {
   const [count, setCount] = useState(0)
 
   return (
-  <div>hhfhfhg</div>
+  <div><Info /></div>
   )
 }
 
