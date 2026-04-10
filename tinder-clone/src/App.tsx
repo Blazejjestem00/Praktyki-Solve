0858a0a5250c9f08d6417f354167a6f3cc232698
Kamil <kamil030309@gmail.com>
2026-04-10 10:15:32 +0200

    T-6 added icons

--- tinder-clone/src/App.tsx
+++ tinder-clone/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import MainView from './components/mainView/mainView'
+import Swipe from './components/swipe/Swipe'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
   <div>    
     <MainView />
+    <Swipe />
   </div>
   )
 }
