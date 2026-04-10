8eba45337d1e0fcd2ea7be328180423bc154c100
Blazej <blazejbialaselc@gmail.com>
2026-04-10 11:58:41 +0200

    tablica z zmiennymi

--- tinder-clone/src/components/description/Description.tsx
+++ tinder-clone/src/components/description/Description.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
+import './Description.css'
 
-
-function MainView() {
+function description() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='description'>
+    <div className='description1'>
       <p>Description</p>
       <p>This is a simple description of the user.</p>
     </div>
   )
 }
 
-export default MainView
+export default description
--- tinder-clone/src/components/mainView/mainView.tsx
+++ tinder-clone/src/components/mainView/mainView.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import Description from '../description/Description'
-
+import List from '../list/List'
 function MainView() {
   const [count, setCount] = useState(0)
 
   return (
   <div>
+    
+        <List />
     <Description />
+    
   </div>
+  
   )
 }
 
