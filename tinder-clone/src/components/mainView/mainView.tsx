378d9db333487d08ba98efb3f25a3f982373ad2a
Maja <majastepien200@gmail.com>
2026-04-10 12:30:36 +0200

    added photo to MainView

--- tinder-clone/src/components/mainView/mainView.tsx
+++ tinder-clone/src/components/mainView/mainView.tsx
@@ -4,11 +4,13 @@ import Info from '../info/info'
 
 import Description from '../description/Description'
 import List from '../list/List'
+import Photo from '../Photo/Photo'
 function MainView() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <div><Photo /></div>
       <div><Info /></div>
       <div>
         <div>
