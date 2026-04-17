a10c74e5b473fa1c25d16ea07a74ce6848452fe9
Barek08 <bartosz.materek1@wp.pl>
2026-04-17 10:35:46 +0200

    Add authentication with Auth0

--- tinder-clone/src/App.tsx
+++ tinder-clone/src/App.tsx
@@ -1,19 +1,27 @@
 import "./App.css";
 import MainView from "./components/MainView/MainView";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import ProfileView from "./components/ProfileView/ProfileView";
 import MatchView from "./components/MatchView/MatchView";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./LoginButton";
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <div>Ładowanie...</div>;
+
+  if (!isAuthenticated) {
+    return <LoginButton />;
+  }
+
   return (
-    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainView />} />
         <Route path="/profile/:id" element={<ProfileView />} />
-
         <Route path="/profile" element={<ProfileView />} />
-        <Route path='/match' element={<MatchView />} />
+        <Route path="/match" element={<MatchView />} />
       </Routes>
     </BrowserRouter>
   );
