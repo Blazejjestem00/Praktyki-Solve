27f3203132cadc6b962ac8b7ed12e46478daee8b
Błażej <blazej@MacBook-Pro-Bazej.local>
2026-04-27 10:53:37 +0200

    T-39-Add-some-features

--- tinder-clone/src/App.tsx
+++ tinder-clone/src/App.tsx
@@ -3,18 +3,40 @@ import MainView from "./components/MainView/MainView";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MatchView from "./components/MatchView/MatchView";
 import PremiumBuyView from "./components/PremiumBuyView/PremiumBuyView";
-import MapView from "./components/MapView/MapView";
 import FaqView from "./components/FaqView/FaqView";
+import SettingsView from "./components/Setings/SettingsView";
+import type { User } from "./services/api";
+import MapView from "./components/MapView/MapView";
+// tymczasowo — pierwszy user = zalogowany
+const currentUser: User = {
+  id: 1,
+  name: "Jan",
+  surname: "Kowalski",
+  age: 25,
+  email: "jan.kowalski@test.pl",
+  interests: ["siłownia", "kolarstwo", "bieganie"],
+  instagram: "jan_fit25",
+  caption: "Hej, jestem Jan 🙂",
+  created_at: "",
+  photoUrl: ["https://randomuser.me/api/portraits/men/1.jpg"],
+};
 
 function App() {
+  const savedTheme = localStorage.getItem("theme") || "dark";
+  document.body.setAttribute("data-theme", savedTheme);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainView />} />
         <Route path="/match" element={<MatchView />} />
         <Route path="/premium" element={<PremiumBuyView />} />
-        <Route path="/map" element={<MapView />} />
         <Route path="/faq" element={<FaqView />} />
+        <Route path="/maps" element={<MapView />} />
+        <Route
+          path="/settings"
+          element={<SettingsView person={currentUser} />}
+        />
       </Routes>
     </BrowserRouter>
   );
